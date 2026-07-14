6a92c2b956f4929df15250caef3c82e2000d45a5
Fyodor Zh <fese@list.ru>
2026-07-14 11:39:35 +0300

    Removed all stuff

--- Pontifex/Abstractions/Transports/NoAck/Raw_old/INoAckRawEndpoint.cs
+++
@@ -1,7 +0,0 @@
-namespace Pontifex.NoAck.Raw_old
-{
-    public interface INoAckRawEndpoint : IBaseEndpoint
-    {
-        int MessageMaxByteSize { get; }
-    }
-}
--- Pontifex/Abstractions/Transports/NoAck/Raw_old/Server/INoAckRawServerSideEndpoint.cs
+++
@@ -1,9 +0,0 @@
-using Pontifex.Utils;
-
-namespace Pontifex.NoAck.Raw_old
-{
-    public interface INoAckRawServerSideEndpoint : INoAckRawEndpoint
-    {
-        SendResult Send(IEndPoint destination, UnionDataList message);
-    }
-}
