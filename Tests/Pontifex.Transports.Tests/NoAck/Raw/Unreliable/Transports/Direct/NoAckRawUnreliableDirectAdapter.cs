b176a0d7200eecd218bf3c18c2d518affdcdffcd
Fyodor Zh <fese@list.ru>
2026-07-30 07:31:14 +0300

    Removed all NoAckRawUnreliable tests

--- Tests/Pontifex.Transports.Tests/NoAck/Raw/Unreliable/Transports/Direct/NoAckRawUnreliableDirectAdapter.cs
+++
@@ -1,9 +0,0 @@
-namespace Pontifex.NoAck.Raw.Unreliable.Tests
-{
-    public class NoAckRawUnreliableDirectAdapter : INoAckRawUnreliableConformanceTestAdapter
-    {
-        public string ImplementationName => "NoAckRawUnreliable.Direct";
-
-        public INoAckRawUnreliableConformanceScope CreateScope() => new NoAckRawUnreliableDirectScope();
-    }
-}
