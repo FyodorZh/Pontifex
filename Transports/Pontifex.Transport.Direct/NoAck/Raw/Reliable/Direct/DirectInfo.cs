4450c22d7db7110ce1ba64d8b686cd9b37d7db8b
Fyodor Zh <fese@list.ru>
2026-08-01 21:30:54 +0300

    removed duplicates of Direct transport

--- Transports/Pontifex.Transport.Direct/NoAck/Raw/Reliable/Direct/DirectInfo.cs
+++
@@ -1,7 +0,0 @@
-// namespace Pontifex.NoAck.Raw.Reliable.Direct
-// {
-//     internal static class DirectInfo
-//     {
-//         public const int MessageMaxByteSize = 1024 * 1024;
-//     }
-// }
